585e20c53bd0ff6edba9b5e1ad1cf2fcebd9f1a3
Daniil Dybka <danil.dybko@gmail.com>
2024-02-08 23:22:28 +0300

    add new web.php

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -17,33 +17,7 @@ use App\Http\Controllers\Posts\CommentController;
 |
 */
 
-//
-// CRUD (create, read, update, delete)
-//
 
-// Route::get('posts', [PostController::class, 'index'])->name('posts.index');
-
-// Route::get('posts/create', [PostController::class, 'create'])->name('posts.create');
-
-// Route::post('posts', [PostController::class, 'store'])->name('posts.store');
-
-// Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
-
-// Route::get('posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
-
-// Route::put('posts/{post}', [PostController::class, 'update'])->name('posts.update');
-
-// Route::delete('posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
-
-//
-// Posts
-//
-
-Route::resource('posts', PostController::class);
-
-Route::put('posts/{post}/like', [PostController::class, 'like'])->name('posts.like');
-
-Route::resource('posts/{post}/comments', CommentController::class);
 
 //
 // Register
