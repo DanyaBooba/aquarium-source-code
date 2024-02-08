18bddd4d2d0f3fdc4e2a858670d313af25564ae6
Daniil Dybka <danil.dybko@gmail.com>
2024-02-08 23:14:56 +0300

    add login and reg controller

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\Posts\CommentController;
 
 /*
@@ -33,8 +35,28 @@ use App\Http\Controllers\Posts\CommentController;
 
 // Route::delete('posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
 
+//
+// Posts
+//
+
 Route::resource('posts', PostController::class);
 
 Route::put('posts/{post}/like', [PostController::class, 'like'])->name('posts.like');
 
 Route::resource('posts/{post}/comments', CommentController::class);
+
+//
+// Register
+//
+
+Route::get('register', [RegisterController::class, 'index'])->name('register.index');
+
+Route::post('register', [RegisterController::class, 'store'])->name('register.store');
+
+//
+// Login
+//
+
+Route::get('login', [LoginController::class, 'index'])->name('login.index');
+
+Route::post('login', [LoginController::class, 'store'])->name('login.store');
