7dc01ae7c9c95ea2e2e5fc28214916b275af0b93
Daniil Dybka <danil.dybko@gmail.com>
2024-02-28 23:19:17 +0300

    work

--- example-app/resources/views/main/about.blade.php
+++ example-app/resources/views/main/about.blade.php
@@ -3,9 +3,5 @@
 @section('page.title', 'О проекте Аквариум')
 
 @section('main.content')
-    <div class="text-center">
-        <h1>
-            О проекте Аквариум
-        </h1>
-    </div>
+    left right
 @endsection
--- example-app/resources/views/main/index.blade.php
+++ example-app/resources/views/main/index.blade.php
@@ -2,12 +2,12 @@
 
 @section('main.content')
     <div class="row g-0 gx-3">
-        <div class="col-sm-6 col-md-8">
+        <div class="col-sm-8 col-md-8 col-lg-8">
             <div class="bg-light p-3" style="border-radius: 8px; min-height: 500px">
                 123
             </div>
         </div>
-        <div class="col-6 col-md-4">
+        <div class="col-sm-4 col-md-4 col-lg-4">
             <div class="bg-dark p-3" style="border-radius: 8px; min-height: 500px">
                 213
             </div>
