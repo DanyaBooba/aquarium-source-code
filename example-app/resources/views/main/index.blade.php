548e132011043b9c1bd22d4a89d79a8190bc8eee
Daniil Dybka <danil.dybko@gmail.com>
2024-02-28 11:13:08 +0300

    add layouts

--- example-app/resources/views/main/index.blade.php
+++ example-app/resources/views/main/index.blade.php
@@ -1,12 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    Hello
-</body>
-</html>
+@extends('layouts.main')
+
+@section('page.title', 'Наш блог')
+
+@section('main.content')
+    <div class="text-center">
+        <h1>
+            Главная страница
+        </h1>
+    </div>
+@endsection
