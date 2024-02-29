5713479f0ee918431df688e81e6658eb399baad6
Daniil Dybka <danil.dybko@gmail.com>
2024-02-29 13:16:22 +0300

    edit css header

--- example-app/resources/views/main/about.blade.php
+++ example-app/resources/views/main/about.blade.php
@@ -3,5 +3,39 @@
 @section('page.title', 'О проекте Аквариум')
 
 @section('main.content')
-    left right
+    <div class="row gx-3">
+        <div class="col-3 p-3">
+            <ul>
+                <li>
+                    <a href="#">
+                        Ссылка 1
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Ссылка 2
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Ссылка 3
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Ссылка 4
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="col-9 p-3">
+            <h1>
+                Информация о проекте Аквариум
+            </h1>
+            <p>
+                Как начинался проект Аквариум? Что имеем на текущий счет? Чего ждать в будущем? Ответим на эти
+                и на другие вопросы.
+            </p>
+        </div>
+    </div>
 @endsection
