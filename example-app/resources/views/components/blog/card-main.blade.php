b2335c00fe24ace6cc37e48b9727bff8ba91c684
Daniil Dybka <danil.dybko@gmail.com>
2024-03-01 19:28:29 +0300

    try to get sign up

--- example-app/resources/views/components/blog/card-main.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="text-white">
-    <div class="bg-dark d-flex flex-column p-4" style="border-radius: 8px; min-height: 300px">
-        <h2 class="display-5">Lorem, ipsum dolor.</h2>
-        <a href="{{ route('blog.show', 1) }}" class="d-flex align-items-center mt-auto mb-2">
-            {{ __('Читать') }}
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right"><path d="m9 18 6-6-6-6"/></svg>
-        </a>
-    </div>
-</div>
--- example-app/resources/views/components/blog/pagination.blade.php
+++ example-app/resources/views/components/blog/pagination.blade.php
@@ -4,7 +4,7 @@
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-left"><path d="m15 18-6-6 6-6"/></svg>
         {{ __('Предыдущая') }}
     </a></li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link active" href="#">1</a></li>
     <li class="page-item"><a class="page-link" href="#">2</a></li>
     <li class="page-item"><a class="page-link" href="#">3</a></li>
     <li class="page-item"><a class="page-link page-link-next" href="#">
