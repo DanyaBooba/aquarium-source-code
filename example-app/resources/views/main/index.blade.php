8e57c7b10203cbde79e9abb9a47c5907271d4206
Daniil Dybka <danil.dybko@gmail.com>
2024-03-01 11:16:21 +0300

    edit background color

--- example-app/resources/views/main/index.blade.php
+++ example-app/resources/views/main/index.blade.php
@@ -15,7 +15,7 @@
 @section('main.content')
     <div class="row g-0 gx-3">
         <div class="col-sm-8 col-md-8 col-lg-8">
-            <div class="bg-light p-3" style="border-radius: 8px; min-height: 500px">
+            <div class="bg-white p-3" style="border-radius: 8px; min-height: 500px">
                 123
             </div>
         </div>
