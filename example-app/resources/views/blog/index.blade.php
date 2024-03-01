a125227879c2d33dd2d14fa9ddb49d0773671d46
Daniil Dybka <danil.dybko@gmail.com>
2024-03-01 22:17:14 +0300

    Work at blog

--- example-app/resources/views/blog/index.blade.php
+++ example-app/resources/views/blog/index.blade.php
@@ -1,6 +1,15 @@
 @extends('layouts.blog')
 
 @section('main.content')
+<div class="d-flex align-items-start" style="margin-bottom: 50px; margin-top: 30px">
+    <h1 class="display-1 col-6 mb-0 text-end pe-5">{{ __('Блог') }}</h1>
+    <div class="d-flex justify-content-end flex-column py-3 ps-5 col-2">
+        <p class="mb-1 small text-end">Информация</p>
+        <p class="mb-1 small text-end">15 посещений</p>
+        <p class="mb-0 small text-end">Блок компании</p>
+    </div>
+</div>
+
 <div class="row row-blog row-cols-3 g-2">
     <x-blog.card />
     <x-blog.card />
--- example-app/resources/views/components/blog/card.blade.php
+++ example-app/resources/views/components/blog/card.blade.php
@@ -1,9 +1,9 @@
-<div class="blog-post">
-    <div class="blog-post-content p-4">
+<a href="{{ route('blog.show', 1) }}" class="blog-post">
+    <span class="blog-post-content p-4">
         <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h2>
-        <a href="{{ route('blog.show', 1) }}" class="d-flex align-items-center mt-auto mb-2">
+        <p class="d-flex align-items-center mt-auto mb-2">
             {{ __('Читать') }}
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right"><path d="m9 18 6-6-6-6"/></svg>
-        </a>
-    </div>
-</div>
+        </p>
+    </span>
+</a>
