7f96cc2d4efd27a7308a403fe71c880a8edfbd0d
Daniil Dybka <danil.dybko@gmail.com>
2024-03-01 15:56:46 +0300

    add copy link

--- example-app/public/js/page/left-bar-anchors.js
+++ example-app/public/js/page/left-bar-anchors.js
@@ -1,3 +1,6 @@
+let svgAnchor =
+    '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-link"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"/><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"/></svg>';
+
 function clearLink(text) {
     var text = text.toLowerCase();
 
@@ -22,8 +25,13 @@ function anchors() {
             "beforeend",
             `<li><a href='#${title.id}'>${title.textContent}</a></li>`
         );
-        title.innerHTML += " <a href='#" + title.id + "'>#</a>";
+        title.classList.add("title-anchor");
+        title.innerHTML = `<a href='#${title.id}' onClick='copyLink("${window.location.href}#${title.id}")'>${svgAnchor}</a>${title.innerHTML}`;
     });
+}
+
+function copyLink(link) {
+    navigator.clipboard.writeText(link);
 }
 
 anchors();
