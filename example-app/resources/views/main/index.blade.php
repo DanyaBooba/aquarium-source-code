f083d6578776348affeabe06802f35a9cfaf40d6
Daniil Dybka <danil.dybko@gmail.com>
2024-03-04 00:54:28 +0300

    work at main page

--- example-app/resources/views/main/index.blade.php
+++ example-app/resources/views/main/index.blade.php
@@ -34,83 +34,84 @@
     <div class="row row-second gx-3">
         <div class="col-sm-4 col-md-4 col-lg-4">
             <div class="p-4">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, incidunt.</p>
+                <h2>Стикеры за регистрацию</h2>
+
             </div>
         </div>
         <div class="col-sm-4 col-md-4 col-lg-4">
             <div class="p-4">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, incidunt.</p>
+                <h2>Удобная авторизация через соцсети</h2>
             </div>
         </div>
         <div class="col-sm-4 col-md-4 col-lg-4">
             <div class="p-4">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, incidunt.</p>
+                <h2>Новости проекта и телеграм канал</h2>
             </div>
         </div>
     </div>
 
     <div class="row row-third row-cols-1 row-cols-lg-3 g-4">
         <div class="col p-4">
-            <img src="{{ asset('img/emoji/fish.png') }}" width="80" class="img-fluid mb-3" alt="Рыбка">
-            <h3 class="mb-2">Тематика</h3>
-            <p class="fs-6 mx-auto col-md-8">
-                Социальная сеть для всех. Поиск друзей и общения.
-            </p>
-        </div>
-        <div class="col p-4">
-            <img src="{{ asset('img/emoji/friends.png') }}" width="80" class="img-fluid mb-3" alt="Друзья">
-            <h3 class="mb-2">Аудитория</h3>
-            <p class="mb-0 fs-6 mx-auto col-md-8">
-                Находите единомышленников и друзей.
-            </p>
-        </div>
-        <div class="col p-4">
-            <img src="{{ asset('img/emoji/dizzy.png') }}" width="80" class="img-fluid mb-3" alt="Звезда">
-            <h3 class="mb-2">Лента</h3>
-            <p class="mb-0 fs-6 mx-auto col-md-8">
-                Ваша лента с новостями и записями друзей.
+            <img src="{{ asset('img/emoji/partying-face.png') }}" width="80" class="img-fluid mb-3" alt="Белый голубь">
+            <h3 class="mb-2">Удобство</h3>
+            <p class="mb-0 fs-6 mx-auto col-md-7">
+                Для мобильных и десктопных версий.
             </p>
         </div>
         <div class="col p-4">
             <img src="{{ asset('img/emoji/robot.png') }}" width="80" class="img-fluid mb-3" alt="Робот">
             <h3 class="mb-2">Безопасность</h3>
-            <p class="mb-0 fs-6 mx-auto col-md-8">
+            <p class="mb-0 fs-6 mx-auto col-md-7">
                 Шифрование и конфиденциальность.
+            </p>
+        </div>
+        <div class="col p-4">
+            <img src="{{ asset('img/emoji/rocket.png') }}" width="80" class="img-fluid mb-3" alt="Ракета">
+            <h3 class="mb-2">Скорость</h3>
+            <p class="mb-0 fs-6 mx-auto col-md-7">
+                Быстрая загрузка даже при слабой сети.
+            </p>
+        </div>
+        <div class="col p-4">
+            <img src="{{ asset('img/emoji/fish.png') }}" width="80" class="img-fluid mb-3" alt="Рыбка">
+            <h3 class="mb-2">Тематика</h3>
+            <p class="fs-6 mx-auto col-md-7">
+                Социальная платформа подойдет для каждого.
+            </p>
+        </div>
+        <div class="col p-4">
+            <img src="{{ asset('img/emoji/friends.png') }}" width="80" class="img-fluid mb-3" alt="Друзья">
+            <h3 class="mb-2">Аудитория</h3>
+            <p class="mb-0 fs-6 mx-auto col-md-7">
+                Большое количество разных интересных людей.
+            </p>
+        </div>
+        <div class="col p-4">
+            <img src="{{ asset('img/emoji/dizzy.png') }}" width="80" class="img-fluid mb-3" alt="Звезда">
+            <h3 class="mb-2">Лента</h3>
+            <p class="mb-0 fs-6 mx-auto col-md-7">
+                Персональная лента с вашими подписками.
             </p>
         </div>
         <div class="col p-4">
             <img src="{{ asset('img/emoji/heart.png') }}" width="80" class="img-fluid mb-3" alt="Сердце">
             <h3 class="mb-2">Эмоции</h3>
-            <p class="mb-0 fs-6 mx-auto col-md-8">
+            <p class="mb-0 fs-6 mx-auto col-md-7">
                 Сохраняйте воспоминания и делитесь с друзьями.
             </p>
         </div>
         <div class="col p-4">
             <img src="{{ asset('img/emoji/shooting-star.png') }}" width="80" class="img-fluid mb-3" alt="Падающая звезда">
             <h3 class="mb-2">Фотографии</h3>
-            <p class="mb-0 fs-6 mx-auto col-md-8">
-                Публикуйте фотографии, расскажите друзьям.
-            </p>
-        </div>
-        <div class="col p-4">
-            <img src="{{ asset('img/emoji/rocket.png') }}" width="80" class="img-fluid mb-3" alt="Ракета">
-            <h3 class="mb-2">Скорость</h3>
-            <p class="mb-0 fs-6 mx-auto col-md-8">
-                Быстрая скорость работы благодаря оптимизации.
+            <p class="mb-0 fs-6 mx-auto col-md-7">
+                Персонализируйте вашу страницу.
             </p>
         </div>
         <div class="col p-4">
             <img src="{{ asset('img/emoji/dove.png') }}" width="80" class="img-fluid mb-3" alt="Белый голубь">
-            <h3 class="mb-2">Свобода</h3>
-            <p class="mb-0 fs-6 mx-auto col-md-8">
-                Выражайте любое мнение, Вы никогда не будете за него заблокированы.
-            </p>
-        </div>
-        <div class="col p-4">
-            <img src="{{ asset('img/emoji/dove.png') }}" width="80" class="img-fluid mb-3" alt="Белый голубь">
-            <h3 class="mb-2">Свобода</h3>
-            <p class="mb-0 fs-6 mx-auto col-md-8">
-                Выражайте любое мнение, Вы никогда не будете за него заблокированы.
+            <h3 class="mb-2">Открытость</h3>
+            <p class="mb-0 fs-6 mx-auto col-md-7">
+                Открытый исходный код <nobr>и API</nobr> для разработчиков.
             </p>
         </div>
     </div>
