d05c1506a4bccd373146f289a9d10f4180d33be0
Daniil Dybka <danil.dybko@gmail.com>
2024-03-12 13:50:32 +0300

    try work at user

--- example-app/database/migrations/2024_03_12_095855_create_users_table.php
+++ example-app/database/migrations/2024_03_12_095855_create_users_table.php
@@ -13,12 +13,25 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->timestamps();
+
+            $table->string('username')->max(100)->min(3);
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('avatar')->nullable();
+
+            $table->boolean('verified')->default(false);
+            $table->boolean('blocked')->default(false);
+
+            $table->string('description')->nullable()->max(100);
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+
+            $table->string('friend_token')->max(30);
+
+            $table->json('settings_notifications');
+
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
         });
     }
 
--- example-app/resources/views/user/search.blade.php
+++ example-app/resources/views/user/search.blade.php
@@ -10,6 +10,13 @@
     </form>
 </div>
 
+<div class="user-search-filters">
+    <button class="btn">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-filter-x"><path d="M13.013 3H2l8 9.46V19l4 2v-8.54l.9-1.055"/><path d="m22 3-5 5"/><path d="m17 3 5 5"/></svg>
+        Очистить фильтры
+    </button>
+</div>
+
 <div class="row row-cols-5 g-3 user-search-users">
     @for($i = 0; $i < 30; $i++)
         <x-user.search-profile />
