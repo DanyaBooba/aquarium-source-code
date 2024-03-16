f630efd804972ffab103ae65ccfae7142acd60a6
Daniil Dybka <danil.dybko@gmail.com>
2024-03-16 14:22:01 +0300

    work at pages

--- example-app/resources/views/layouts/simple.blade.php
+++ example-app/resources/views/layouts/simple.blade.php
@@ -1,11 +1,17 @@
 @extends('layouts.base')
 
+@once
+    @push('css')
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/simple/index.css') }}" />
+    @endpush
+@endonce
+
 @section('body')
 <body>
     <div class="d-flex flex-column justify-content-between min-vh-100">
         <main class="flex-grow-1 py-1">
             <section>
-                <div class="container mt-2">
+                <div class="container container-simple mt-2">
                     @yield('simple.content')
                 </div>
             </section>
--- example-app/resources/views/main/index.blade.php
+++ example-app/resources/views/main/index.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.main')
 
+@section('page.title', 'Социальная сеть')
+
 @push('css')
 <link rel="stylesheet" type="text/css" href="{{ asset('css/main-page/index.css') }}" />
 @endpush
