bd669ffb0638eaca2987a453fb919c7762f9ca5d
Daniil Dybka <danil.dybko@gmail.com>
2024-03-20 04:55:57 +0300

    en trans

--- example-app/public/js/page/code.js
+++ example-app/public/js/page/code.js
@@ -9,7 +9,9 @@ function findCode() {
         item.id = id;
         item.insertAdjacentHTML(
             "afterbegin",
-            `<a href="#${id}" onClick="codeCopy('${id}')" id="link-${id}" class="code-copy"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="14" height="14" x="8" y="8" rx="2" ry="2"/><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/></svg><span>Скопировать</span></a>`
+            `<a href="#${id}" onClick="codeCopy('${id}')" id="link-${id}" class="code-copy"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="14" height="14" x="8" y="8" rx="2" ry="2"/><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/></svg><span>${textCodeCopy(
+                false
+            )}</span></a>`
         );
 
         index += 1;
@@ -40,7 +42,12 @@ function changeCodeCopyText(id, status) {
 }
 
 function textCodeCopy(status) {
-    return status ? "Скопировано!" : "Скопировать";
+    let lang = document.querySelector("html").lang;
+    if (lang === "ru") {
+        return status ? "Скопировано!" : "Скопировать";
+    } else {
+        return status ? "Copied!" : "Copy";
+    }
 }
 
 findCode();
