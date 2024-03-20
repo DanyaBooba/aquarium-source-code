9730548d6c22f4f85a1fb62baf7fc54e103867a7
Daniil Dybka <danil.dybko@gmail.com>
2024-03-20 07:08:45 +0300

    work at app

--- example-app/resources/views/user/settings/appearance.blade.php
+++ example-app/resources/views/user/settings/appearance.blade.php
@@ -10,7 +10,7 @@
 <form action="" method="post">
     @csrf
     <div>
-        <label for="username" class="form-label">Имя пользователя</label>
+        <label for="username" class="form-label">Фотография</label>
         <input type="text" name="username" class="form-control" id="username" placeholder="К примеру, superman" onInput="data()" value="user10">
         <p>Может содержать только латинские буквы в нижнем регистре.</p>
     </div>
@@ -25,4 +25,106 @@
 
     <button class="btn btn-success" type="submit">{{ __('Сохранить') }}</button>
 </form>
+
+<form class="needs-validation person-settings-form-image w-100" action="/api/php/person/edit/edit-icon.php" method="post" novalidate>
+    <div class="row row-cols-2 row-cols-lg-4 g-2">
+        <div class="col">
+            <input class="form-check-input visually-hidden" type="radio" name="icon" id="icon1" value="1" <?php echo $formicon[1] ?>>
+            <label class="list-group-item" for="icon1">
+                <img src="/app/img/users/icons/<?php echo $formmale ?>1.png" id="imgicon1">
+            </label>
+        </div>
+        <div class="col">
+            <input class="form-check-input visually-hidden" type="radio" name="icon" id="icon2" value="2" <?php echo $formicon[2] ?>>
+            <label class="list-group-item" for="icon2">
+                <img src="/app/img/users/icons/<?php echo $formmale ?>2.png" id="imgicon2">
+            </label>
+        </div>
+        <div class="col">
+            <input class="form-check-input visually-hidden" type="radio" name="icon" id="icon3" value="3" <?php echo $formicon[3] ?>>
+            <label class="list-group-item" for="icon3">
+                <img src="/app/img/users/icons/<?php echo $formmale ?>3.png" id="imgicon3">
+            </label>
+        </div>
+        <div class="col">
+            <input class="form-check-input visually-hidden" type="radio" name="icon" id="icon4" value="4" <?php echo $formicon[4] ?>>
+            <label class="list-group-item" for="icon4">
+                <img src="/app/img/users/icons/<?php echo $formmale ?>4.png" id="imgicon4">
+            </label>
+        </div>
+        <div class="col">
+            <input class="form-check-input visually-hidden" type="radio" name="icon" id="icon5" value="5" <?php echo $formicon[5] ?>>
+            <label class="list-group-item" for="icon5">
+                <img src="/app/img/users/icons/<?php echo $formmale ?>5.png" id="imgicon5">
+            </label>
+        </div>
+        <div class="col">
+            <input class="form-check-input visually-hidden" type="radio" name="icon" id="icon6" value="6" <?php echo $formicon[6] ?>>
+            <label class="list-group-item" for="icon6">
+                <img src="/app/img/users/icons/<?php echo $formmale ?>6.png" id="imgicon6">
+            </label>
+        </div>
+        <div class="col">
+            <input class="form-check-input visually-hidden" type="radio" name="icon" id="icon7" value="7" <?php echo $formicon[7] ?>>
+            <label class="list-group-item" for="icon7">
+                <img src="/app/img/users/icons/<?php echo $formmale ?>7.png" id="imgicon7">
+            </label>
+        </div>
+        <input type="text" class="d-none" name="ismale" id="ismale" value="<?php echo $sex ?>">
+    </div>
+    <button class="btn btn-primary col-md-6 mt-2" type="submit">
+        Сохранить изменения
+    </button>
+</form>
+
+<form class="needs-validation person-settings-form-image form-image-bg w-100" action="/api/php/person/edit/edit-bg.php" method="post" novalidate>
+    <div class="row row-cols-1 row-cols-lg-2 g-2">
+        <div class="col">
+            <input class="form-check-input visually-hidden" type="radio" name="bg" id="bg1" value="1" <?php echo $formbg[1] ?>>
+            <label class="list-group-item" for="bg1">
+                <img src="/app/img/users/bg/BG1.jpg">
+            </label>
+        </div>
+        <div class="col">
+            <input class="form-check-input visually-hidden" type="radio" name="bg" id="bg2" value="2" <?php echo $formbg[2] ?>>
+            <label class="list-group-item" for="bg2">
+                <img src="/app/img/users/bg/BG2.jpg">
+            </label>
+        </div>
+        <div class="col">
+            <input class="form-check-input visually-hidden" type="radio" name="bg" id="bg3" value="3" <?php echo $formbg[3] ?>>
+            <label class="list-group-item" for="bg3">
+                <img src="/app/img/users/bg/BG3.jpg">
+            </label>
+        </div>
+        <div class="col">
+            <input class="form-check-input visually-hidden" type="radio" name="bg" id="bg4" value="4" <?php echo $formbg[4] ?>>
+            <label class="list-group-item" for="bg4">
+                <img src="/app/img/users/bg/BG4.jpg">
+            </label>
+        </div>
+        <div class="col">
+            <input class="form-check-input visually-hidden" type="radio" name="bg" id="bg5" value="5" <?php echo $formbg[5] ?>>
+            <label class="list-group-item" for="bg5">
+                <img src="/app/img/users/bg/BG5.jpg">
+            </label>
+        </div>
+        <div class="col">
+            <input class="form-check-input visually-hidden" type="radio" name="bg" id="bg6" value="6" <?php echo $formbg[6] ?>>
+            <label class="list-group-item" for="bg6">
+                <img src="/app/img/users/bg/BG6.jpg">
+            </label>
+        </div>
+        <div class="col">
+            <input class="form-check-input visually-hidden" type="radio" name="bg" id="bg7" value="7" <?php echo $formbg[7] ?>>
+            <label class="list-group-item" for="bg7">
+                <img src="/app/img/users/bg/BG7.jpg">
+            </label>
+        </div>
+    </div>
+    <button class="btn btn-primary col-md-6 mt-2" type="submit">
+        Сохранить изменения
+    </button>
+</form>
+
 @endsection
--- example-app/resources/views/user/settings/index.blade.php
+++ example-app/resources/views/user/settings/index.blade.php
@@ -38,7 +38,7 @@
         </li> --}}
         <li class="fs-5 list-group-item">
             <a href="{{ route('user.settings.appearance') }}" class="settings-personalization">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-paintbrush-2"><path d="M14 19.9V16h3a2 2 0 0 0 2-2v-2H5v2c0 1.1.9 2 2 2h3v3.9a2 2 0 1 0 4 0Z"/><path d="M6 12V2h12v10"/><path d="M14 2v4"/><path d="M10 2v2"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-image"><rect width="18" height="18" x="3" y="3" rx="2" ry="2"/><circle cx="9" cy="9" r="2"/><path d="m21 15-3.086-3.086a2 2 0 0 0-2.828 0L6 21"/></svg>
                 Персонализация
             </a>
         </li>
