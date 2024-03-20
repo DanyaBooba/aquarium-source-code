6f5384e757d2310ed362af47674e4211d550153e
Daniil Dybka <danil.dybko@gmail.com>
2024-03-20 18:08:11 +0300

    work at appe

--- example-app/resources/views/user/settings/appearance.blade.php
+++ example-app/resources/views/user/settings/appearance.blade.php
@@ -28,12 +28,23 @@
 
 <form action="" method="post" class="form-settings-image" novalidate>
     <h2 class="h3">Аватарка</h2>
-    <div class="row">
+    <div class="row row-settings-avatar">
         @for($i = 1; $i <= 7; $i++)
         <div class="col">
             <input class="form-check-input visually-hidden" type="radio" name="icon" id="icon{{ $i }}" value="{{ $i }}" {{ $i == 1 ? "checked" : "" }}>
             <label class="list-group-item" for="icon{{ $i }}">
                 <img src="{{ asset("/img/user/logo/MAN$i.png") }}">
+            </label>
+        </div>
+        @endfor
+    </div>
+    <h2 class="h3">Шапка</h2>
+    <div class="row row-settings-cap">
+        @for($i = 1; $i <= 11; $i++)
+        <div class="col">
+            <input class="form-check-input visually-hidden" type="radio" name="bg" id="bg{{ $i }}" value="{{ $i }}" {{ $i == 1 ? "checked" : "" }}>
+            <label class="list-group-item" for="bg{{ $i }}">
+                <img src="{{ asset("/img/user/bg/BG$i.jpg") }}">
             </label>
         </div>
         @endfor
