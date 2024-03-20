47ecc729e0a5f91f72b8cd9f2a4e94c51eb416db
Daniil Dybka <danil.dybko@gmail.com>
2024-03-20 19:12:55 +0300

    work at achivs

--- example-app/resources/views/user/achievements.blade.php
+++ example-app/resources/views/user/achievements.blade.php
@@ -3,15 +3,18 @@
 @section('page.title', __('Достижения'))
 
 @section('user.content')
+
 <h1>{{ __('Достижения') }}</h1>
-<ul class="list-achievements">
+<div class="list-achievements row row-cols-md-3">
     @for($i = 1; $i < 15; $i++)
-    <li>
-        <img src="{{ asset("/img/user/achivs/achiv-$i.jpg") }}" alt="">
-        <p>
-            Достижение {{ $i }}
-        </p>
-    </li>
+    <div>
+        <span>
+            <img src="{{ asset("/img/user/achivs/achiv-$i.jpg") }}" alt="">
+        </span>
+        <x-circle-text-simple>
+            прикол вообще!
+        </x-circle-text-simple>
+    </div>
     @endfor
 </ul>
 @endsection
