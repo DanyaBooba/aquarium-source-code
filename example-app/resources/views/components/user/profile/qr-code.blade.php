68c63beef8c942ff00d2891fd7513b20f48cbf92
Daniil Dybka <danil.dybko@gmail.com>
2024-03-23 03:54:28 +0300

    new qr code

--- example-app/resources/views/components/user/profile/qr-code.blade.php
+++ example-app/resources/views/components/user/profile/qr-code.blade.php
@@ -1,4 +1,4 @@
-<div class="modal modal-qrcode fade" id="qrCodeModal" tabindex="-1" aria-labelledby="qrCodeModalLabel" aria-hidden="true">
+<div class="modal show modal-qrcode fade" style="display: block; padding-left: 0px;" id="qrCodeModal" tabindex="-1" aria-labelledby="qrCodeModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
@@ -22,7 +22,32 @@
 </div>
 
 <script src="{{ asset('js/user/qrcode.min.js') }}"></script>
-<script type="text/javascript">
+<script type="text/javascript" src="https://unpkg.com/qr-code-styling@1.5.0/lib/qr-code-styling.js"></script>
+<script>
+    const qrCode = new QRCodeStyling({
+        width: 300,
+        height: 300,
+        type: "svg",
+        data: "https://dev.to/luckynkosi/",
+        image: "https://d2fltix0v2e0sb.cloudfront.net/dev-rainbow.svg",
+        dotsOptions: {
+            color: "#4267b2",
+            type: "rounded"
+        },
+        backgroundOptions: {
+            color: "#e9ebee",
+        },
+        imageOptions: {
+            crossOrigin: "anonymous",
+            margin: 20
+        }
+    });
+
+    qrCode.append(document.getElementById("qr-code-generator"));
+
+    // qrCode.download({ name: "qr", extension: "svg" });
+</script>
+{{-- <script type="text/javascript">
     new QRCode(document.getElementById("qr-code-generator"), {
             text: "{{ $nickname ? route('user.show.nickname', $nickname) : route('user.show.id', $id) }}",
             width: 1024,
@@ -31,5 +56,4 @@
             colorLight: "#ffffff",
             correctLevel: QRCode.CorrectLevel.Q,
         });
-
-</script>
+</script> --}}
