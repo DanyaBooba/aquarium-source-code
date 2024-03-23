e73d38c6ddd539290a677344549babe52b76c623
Daniil Dybka <danil.dybko@gmail.com>
2024-03-23 03:29:51 +0300

    first qr code

--- example-app/resources/views/components/user/profile/qr-code.blade.php
+++ example-app/resources/views/components/user/profile/qr-code.blade.php
@@ -2,13 +2,18 @@
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
+                @if($nickname)
+                <h4>
+                    {{ $nickname }}
+                </h4>
+                @endif
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <div id="qr-code-generator">
-
-                </div>
-                <button type="button" class="btn btn-secondary" onClick="buttonCopyURL('{{ $nickname ? route('user.show.nickname', $nickname) : route('user.show.id', $id) }}')">
+                <div id="qr-code-generator" class="qr-code-generator"></div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-dark" onClick="buttonCopyURL('{{ $nickname ? route('user.show.nickname', $nickname) : route('user.show.id', $id) }}')">
                     Скопировать
                 </button>
             </div>
@@ -18,13 +23,11 @@
 
 <script src="{{ asset('js/user/qrcode.min.js') }}"></script>
 <script type="text/javascript">
-    // new QRCode(document.getElementById("qr-code-generator"), "{{ $nickname ? route('user.show.nickname', $nickname) : route('user.show.id', $id) }}");
-
     new QRCode(document.getElementById("qr-code-generator"), {
-            text: "https://example.com",
+            text: "{{ $nickname ? route('user.show.nickname', $nickname) : route('user.show.id', $id) }}",
             width: 1024,
             height: 1024,
-            colorDark: "#575FCF",
+            colorDark: "#323791",
             colorLight: "#ffffff",
             correctLevel: QRCode.CorrectLevel.Q,
         });
