f531cfd2b22de8fd2229cce570c41b268eda86d5
Daniil Dybka <danil.dybko@gmail.com>
2024-05-01 16:33:01 +0300

    add form to add post

--- example-app/resources/views/user/addpost.blade.php
+++ example-app/resources/views/user/addpost.blade.php
@@ -1,7 +1,18 @@
-@extends('layouts.user')
+@extends('layouts.addpost')
 
-@section('page.title', __('Добавить пост'))
+@section('page.title', 'Настройки')
 
-@section('user.content')
-Add post
+@section('addpost.content')
+<div class="container-settings-main">
+    <h1>{{ __('Добавить пост') }}</h1>
+    <div class="addpost-container">
+        <form action="" method="post">
+            <div>
+                <input type="text">
+            </div>
+            <input id="x" type="hidden" name="content">
+            <trix-editor input="x"></trix-editor>
+            </form>
+    </div>
+</div>
 @endsection
