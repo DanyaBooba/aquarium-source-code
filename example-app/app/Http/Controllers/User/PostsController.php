a9b2b9212772dbbc827f0124f50ba5fa39f4b5df
Daniil Dybka <danil.dybko@gmail.com>
2024-05-02 00:47:22 +0300

    Update PostsController.php

--- example-app/app/Http/Controllers/User/PostsController.php
+++ example-app/app/Http/Controllers/User/PostsController.php
@@ -12,8 +12,13 @@ class PostsController extends Controller
         return view('user.addpost');
     }
 
-    public function post()
+    public function post(Request $request)
     {
+        $validated = $request->validate([
+            'email' => ['required', 'string', 'max:254', 'min: 3', 'email'],
+            'password' => ['required', 'string', 'min:3', 'max:254'],
+        ]);
+
         dd('post');
     }
 }
