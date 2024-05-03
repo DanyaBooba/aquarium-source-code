a5077ae26f728b44dbe2c8e956869beeaf51ffa4
Daniil Dybka <danil.dybko@gmail.com>
2024-05-03 22:22:33 +0300

    ready for wowjs

--- example-app/resources/views/includes/header.blade.php
+++ example-app/resources/views/includes/header.blade.php
@@ -4,7 +4,7 @@
             <a href="{{ route('main') }}" aria-label="{{ __('Главная страница') }}" class="header-content-logo link-body-emphasis">
                 <x-header-logo />
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler px-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="4" x2="20" y1="12" y2="12"/><line x1="4" x2="20" y1="6" y2="6"/><line x1="4" x2="20" y1="18" y2="18"/></svg>
             </button>
             <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
