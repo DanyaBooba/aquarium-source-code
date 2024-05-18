3d049d0e31d201991149acd888e4ced323b46f3e
Daniil Dybka <danil.dybko@gmail.com>
2024-05-18 03:28:47 +0300

    start work at acc

--- example-app/resources/views/main/accessibility.blade.php
+++ example-app/resources/views/main/accessibility.blade.php
@@ -1,13 +1,31 @@
 @extends('layouts.main.main')
 
-@section('page.title', 'Социальная сеть')
+@section('page.title', 'Цифровая доступность')
 
 @push('css')
-<link rel="stylesheet" type="text/css" href="{{ asset('css/main-page/include.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('css/faq/include.css') }}" />
 @endpush
 
 @section('main.content')
+    <div class="row gx-3">
+        <div class="col-3 p-3">
+            <x-page.title-anchor />
+        </div>
+        <div class="col-7 p-3">
+            <h1>
+                {{ __('Цифровая доступность') }}
+            </h1>
+            <p class="fs-5">
+                {{ __('...') }}
+            </p>
+            <h2>{{ __('...') }}</h2>
 
-accessibility
+        </div>
+    </div>
 
+    <x-button-top />
 @endsection
+
+@push('js')
+    <script src="{{ asset('js/page/left-bar-anchors.js') }}"></script>
+@endpush
