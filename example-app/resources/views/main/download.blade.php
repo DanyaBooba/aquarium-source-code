5f2012519e51ea8f0f2453945e0f7806a3e51482
Daniil Dybka <danil.dybko@gmail.com>
2024-05-18 03:26:35 +0300

    add new page

--- example-app/resources/views/main/download.blade.php
+++ example-app/resources/views/main/download.blade.php
@@ -11,14 +11,17 @@
 
 <div class="page-download">
     <div class="p-3 page-download__container">
-        <div class="d-flex justify-content-center pb-2 mt-auto" style="margin-top: -10px">
-            <a href="{{ route('main.download') }}" aria-label="{{ __('Скачать для Android') }}" style="margin-right: -30px; margin-top: 10px;">
-                <img src="{{ asset('img/main/android-' . (App::isLocale('ru') ? "ru" : "en") . '.png') }}" class="img-fluid" width="200" aria-label="{{ __('Телефон со страницей сайта') }}">
+        <div class="d-flex align-items-end justify-content-center pb-2 mt-auto" style="margin-top: -10px">
+            <a href="{{ route('main') }}" aria-label="{{ __('Перейти в веб-интерфейс') }}" style="margin-right: -100px; margin-top: 80px;">
+                <img src="{{ asset('img/main/macbook/macbook-' . (App::isLocale('ru') ? "ru" : "en") . '.png') }}" class="img-fluid" width="600" aria-label="{{ __('Ноутбук со страницей сайта') }}">
             </a>
-            <a href="{{ route('main.download') }}" aria-label="{{ __('Скачать для iOS') }}">
+            <a href="{{ route('main') }}" aria-label="{{ __('Перейти в веб-интерфейс') }}">
                 <img src="{{ asset('img/main/iphone-' . (App::isLocale('ru') ? "ru" : "en") . '.png') }}" class="img-fluid" width="210" aria-label="{{ __('Телефон со страницей сайта') }}">
             </a>
         </div>
+        <h2>
+            Веб-интерфейс
+        </h2>
     </div>
 </div>
 
