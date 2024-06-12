c007f477068e41544c9bbe7443da15a3448f2801
Daniil Dybka <danil.dybko@gmail.com>
2024-06-12 22:34:25 +0300

    work at project

---
+++ example-app/resources/views/includes/user/header.blade.php
@@ -0,0 +1,10 @@
+<header class="container">
+    <div class="header__logo">
+        <x-header-logo />
+    </div>
+    <div class="header__meta">
+        <a href="{{ route('user.search') }}" class="{{ header_route_active_link('user.search') }}" title="{{ __('Поиск') }}">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg>
+        </a>
+    </div>
+</header>
