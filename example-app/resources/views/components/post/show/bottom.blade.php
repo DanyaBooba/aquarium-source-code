0216f96c369a23eed3889b9f4b55a970ee02b647
Daniil Dybka <danil.dybko@gmail.com>
2024-07-11 20:28:41 +0300

    work at posts

---
+++ example-app/resources/views/components/post/show/bottom.blade.php
@@ -0,0 +1,4 @@
+<div class="container">
+    лайк
+    и тд
+</div>
--- example-app/resources/views/user/show-post.blade.php
+++ example-app/resources/views/user/show-post.blade.php
@@ -1,31 +1,31 @@
 @extends('layouts.user.settings')
 
 @push('css')
-<link rel="stylesheet" type="text/css" href="{{ asset('css/posts/include.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/posts/include.css') }}" />
 @endpush
 
 @section('page.title', __('Пост'))
 
 @section('user.alert')
-@if($active == 0)
-<x-user.alert :close=false>
-    {{ __('Запись находится на модерации') }}
-</x-user.alert>
-@elseif($active == -1)
-<x-user.alert :close=false>
-    {{ __('Запись не одобрена к публикации') }}
-</x-user.alert>
-@endif
+    @if ($active == 0)
+        <x-user.alert :close=false>
+            {{ __('Запись находится на модерации') }}
+        </x-user.alert>
+    @elseif($active == -1)
+        <x-user.alert :close=false>
+            {{ __('Запись не одобрена к публикации') }}
+        </x-user.alert>
+    @endif
 @endsection
 
 @section('settings.content')
 
-<div class="profile-posts">
+    <x-post.show.header />
 
-</div>
+    <div>
+        {!! $post->message !!}
+    </div>
 
-<div>
-    {!! $post->message !!}
-</div>
+    <x-post.show.bottom />
 
 @endsection
