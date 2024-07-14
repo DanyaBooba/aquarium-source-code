97ef69e0337674619c0612288f2e6b1b08518236
Daniil Dybka <danil.dybko@gmail.com>
2024-07-14 17:21:38 +0300

    work at site

--- example-app/resources/views/components/user/profile-modal.blade.php
+++ example-app/resources/views/components/user/profile-modal.blade.php
@@ -24,7 +24,7 @@
                                 @endforeach
                             </ul>
                         @else
-                            achivs
+                            @json($data)
                         @endif
                     </div>
                 </div>
