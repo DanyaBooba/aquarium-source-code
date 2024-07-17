03e42a90a71eb598673b8293e5eb876d3d43f7d8
Daniil Dybka <danil.dybko@gmail.com>
2024-07-17 22:34:18 +0300

    change color

--- example-app/routes/blog.php
+++ example-app/routes/blog.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BlogController;
 
-Route::prefix('blog')->group(function () {
-    Route::get('/', [BlogController::class, 'index'])->name('blog.index');
+// Route::prefix('blog')->group(function () {
+//     Route::get('/', [BlogController::class, 'index'])->name('blog.index');
 
-    Route::get('{id}', [BlogController::class, 'show'])->name('blog.show');
-});
+//     Route::get('{id}', [BlogController::class, 'show'])->name('blog.show');
+// });
