bee4235535758b24faee8621e865ae292fe4fb16
Daniil Dybka <danil.dybko@gmail.com>
2024-07-25 05:46:30 +0300

    work at profile

--- example-app/resources/views/layouts/user/user.blade.php
+++ example-app/resources/views/layouts/user/user.blade.php
@@ -1,28 +1,29 @@
 @extends('layouts.base')
 
 @push('css')
-<link rel="stylesheet" type="text/css" href="{{ asset('css/user/index.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/user/index.css') }}" />
 @endpush
 
 @section('body')
-<body class="container-fluid">
-    <div class="row row-user min-vh-100">
-        <section class="row-user-bar">
-            @include('includes.user.bar')
-        </section>
-        <main class="flex-grow-1 row-user-content px-0">
-            @include('includes.user.header')
-            @yield('user.alert')
-            <section class="py-3">
-                <div class="container">
-                    @yield('user.content')
-                </div>
-            </section>
-        </main>
-        @include('includes.user.nav')
-    </div>
 
-    <script src="{{ asset('js/user/alert.js') }}"></script>
-    <script src="{{ asset('js/bootstrap.js') }}"></script>
-</body>
+    <body class="container-fluid">
+        <div class="row row-user min-vh-100">
+            <section class="row-user-bar">
+                @include('includes.user.bar')
+            </section>
+            <main class="flex-grow-1 row-user-content">
+                @include('includes.user.header')
+                @yield('user.alert')
+                <section class="py-3">
+                    <div class="container">
+                        @yield('user.content')
+                    </div>
+                </section>
+            </main>
+            @include('includes.user.nav')
+        </div>
+
+        <script src="{{ asset('js/user/alert.js') }}"></script>
+        <script src="{{ asset('js/bootstrap.js') }}"></script>
+    </body>
 @endsection
