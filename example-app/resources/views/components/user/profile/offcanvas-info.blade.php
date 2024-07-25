f0a5d86e9ad301249705006955ed0530412b0621
Daniil Dybka <danil.dybko@gmail.com>
2024-07-26 00:17:22 +0300

    work at ui

--- example-app/resources/views/components/user/profile/offcanvas-info.blade.php
+++ example-app/resources/views/components/user/profile/offcanvas-info.blade.php
@@ -5,7 +5,7 @@
 <div class="offcanvas offcanvas-bottom" tabindex="-1" id="canvasInfo" aria-labelledby="canvasInfoLabel">
     <div class="offcanvas-header">
         <h5 class="offcanvas-title" id="canvasInfoLabel">
-            {{ __('Информация') }}
+            {{ __('Информация о профиле') }}
         </h5>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
