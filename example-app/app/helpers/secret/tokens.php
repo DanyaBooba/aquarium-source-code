c44edc98df38c7b6e0fc00d27ba93a67028908c3
Daniil Dybka <creagoooff@gmail.com>
2025-03-11 03:38:07 +0300

    Fix

--- example-app/app/helpers/secret/tokens.php
+++ example-app/app/helpers/secret/tokens.php
@@ -1,23 +1,23 @@
 <?php
 
-define('YANDEX_CLIENT_ID', '51193a98780a440b9bf8e93ae9971897');
-define('YANDEX_CLIENT_SECRET', 'bc09d35a23c0413bacf3012373ea1d46');
+define('YANDEX_CLIENT_ID', '');
+define('YANDEX_CLIENT_SECRET', '');
 define('YANDEX_REDIRECT_URI_LOGIN', 'https://aquariumsocial.ru/signin/yandex');
 define('YANDEX_REDIRECT_URI_SECOND', 'https://aquariumsocial.ru/second/signin/yandex');
 
-define('GOOGLE_CLIENT_ID', '386822047573-bg1dqjul0g080t4h2f7atcelregqjepi.apps.googleusercontent.com');
-define('GOOGLE_CLIENT_SECRET', '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');
+define('GOOGLE_CLIENT_ID', '');
+define('GOOGLE_CLIENT_SECRET', '');
 define('GOOGLE_REDIRECT_URI_LOGIN', 'https://aquariumsocial.ru/signin/google');
 define('GOOGLE_REDIRECT_URI_SECOND', 'https://aquariumsocial.ru/second/signin/google');
 
-define('GITHUB_CLIENT_ID_FIRST', 'Ov23liz5ufPkrZ3cb0kA');
-define('GITHUB_CLIENT_SECRET_FIRST', '7545d150e79fbdb0f4f8b06d519a1ee41f6096dc');
+define('GITHUB_CLIENT_ID_FIRST', '');
+define('GITHUB_CLIENT_SECRET_FIRST', '');
 define('GITHUB_CALLBACK_URL_FIRST', 'https://aquariumsocial.ru/signin/github');
-define('GITHUB_CLIENT_ID_SECOND', 'Ov23lis7smO6TjaTsRYv');
-define('GITHUB_CLIENT_SECRET_SECOND', 'fb3ced0d95240b6f9bbc761ca2fadb136829244d');
+define('GITHUB_CLIENT_ID_SECOND', '');
+define('GITHUB_CLIENT_SECRET_SECOND', '');
 
-define('VK_APP_ID', '51805978');
-define('VK_CLIENT_SECRET', 'xUSY9yTOpu6DnuBVA2PV');
+define('VK_APP_ID', '');
+define('VK_CLIENT_SECRET', '');
 define('VK_REDIRECT_URI_LOGIN', 'https://aquariumsocial.ru/signin/vk');
 // define('VK_REDIRECT_URI_LOGIN', env('APP_URL') . '/signin/vk');
 define('VK_STATE', '/user');
